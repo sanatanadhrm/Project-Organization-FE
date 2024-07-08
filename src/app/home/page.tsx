99dd4c710b0d4b7e7a90a892ac61bfa084dad1f6
Natan <zantanad.24@gmail.com>
2024-07-08 15:41:40 +0700

    push

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1>Page</h1>
+    </div>
+  );
+};
+export default page;
