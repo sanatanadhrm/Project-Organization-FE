b0a8a0889db139f85c060ad76b57bba2d975e282
Mohammad Arif Fadhilah <ariffadhilah124@gmail.com>
2024-07-20 23:29:05 +0700

    responsive: improve onboarding page responsive to tablet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,39 +8,43 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <Navbar />
-      <section className="w-full h-full mt-32">
+      <section className="w-full mt-36">
         <div className="flex items-center justify-center relative h-20">
-          <Badge variant="outline" className="sm:w-24 sm:h-10 w-[100px] h-8 flex-col justify-center border-[1px] border-black text-sm sm:text-base">Home</Badge>
+          <Badge variant="outline" className="md:w-32 md:h-12 w-24 h-10 flex-col justify-center border-[1px] border-black text-sm md:text-base">Home</Badge>
           <Image
             src={orangeThreeLine}
-            className="absolute -mt-14 -mr-32 w-[40px] h-[50px]"
+            className="absolute -mt-14 -mr-32 md:-mt-16 md:-mr-36 w-[40px] h-[50px]"
             alt="Logo"
             width={40}
             height={50} />
         </div>
-        <div className="h-fit flex-col justify-between relative">
-          <p className="font-semibold  text-[25px] lg:text-6xl sm:text-5xl text-center">I&lsquo;m
+        <div className="h-full flex-col justify-between relative">
+          <p className="font-semibold text-2xl md:text-5xl lg:text-6xl text-center md:mt-3">I&lsquo;m
             <span className="text-[#FE853A]"> OrgControl </span>
           </p>
-          <p className="font-semibold text-[25px] lg:text-6xl sm:text-5xl text-center sm:mt-4">
+          <p className="font-semibold text-2xl md:text-5xl lg:text-6xl text-center mt-2 md:mt-4">
             Organization Management
           </p>
           <div className="relative">
+
+            {/* Desktop Image */}
             <Image
               src={sectionImageDesktop}
-              className="mx-auto hidden sm:block object-cover w-1/2 h-auto"
+              className="mx-auto hidden lg:block object-cover w-2/3 h-auto"
               alt="Logo"
               width={810}
               height={600}
               priority />
-            <div className="absolute w-full text-center sm:bottom-16 top-2/3">
+            <div className="absolute w-full text-center top-[70%]">
               <Link href="/signup" >
-                <Button variant="outline" className="bg-[#FE853A] rounded-full text-white font-medium sm:text-xl text-base sm:w-[267px] sm:h-[62px] w-[238px] h-[52px] shadow-gray-500 shadow-sm border-none">Getting Started</Button>
+                <Button variant="outline" className="bg-[#FE853A] rounded-full text-white font-medium text-base md:text-2xl md:w-80 md:h-20 w-60 h-12 shadow-gray-500 shadow-sm border-none">Getting Started</Button>
               </Link>
             </div>
+
+            {/* Mobile Image */}
             <Image
               src={sectionImageMobile}
-              className="mx-auto block sm:hidden object-cover"
+              className="w-full h-auto mx-auto block lg:hidden object-cover"
               alt="Logo"
               width={810}
               height={600} />
